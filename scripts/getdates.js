51aa6fa53e0859b1f56f426e5b00d3586c0d9fc0
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2024-09-21 21:39:52 -0700

    finished site

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// get current year
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// get last modified date
+document.getElementById("lastModified").textContent =
+  "Last Modified: " + document.lastModified;
