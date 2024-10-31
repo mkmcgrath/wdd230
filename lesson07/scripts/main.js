b7cac6f18adc735061ecfc2d23d2cede27e6a24f
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2024-10-31 14:35:03 -0700

    added lazyload stuff

---
+++ lesson07/scripts/main.js
@@ -0,0 +1,4 @@
+// Display the document’s last modified date in the footer
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("lastModified").textContent = document.lastModified;
+});
