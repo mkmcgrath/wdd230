c7436980c4b735d88916e900c98f573207b1236b
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2024-11-01 12:02:58 -0700

    fixed silly missing equals sign

--- scripts/visitcounter.js
+++ scripts/visitcounter.js
@@ -2,7 +2,7 @@ const visitsDisplay = document.querySelector(".visits");
 
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
-if ((numVisits = 0)) {
+if (numVisits == 0) {
   visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
 } else {
   visitsDisplay.textContent = numVisits;
