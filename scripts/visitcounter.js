f2a065cfa124f5ffaba73931f8c6f44e868ee311
mkmcgrath <matthew.k.mcgrath3@gmail.com>
2024-11-01 11:56:16 -0700

    fixing bugs

--- scripts/visitcounter.js
+++ scripts/visitcounter.js
@@ -2,10 +2,10 @@ const visitsDisplay = document.querySelector(".visits");
 
 let numVisits = Number(window.localStorage.getItem("numVisits- ls")) || 0;
 
-if (numVisits !== 0) {
-  visitsDisplay.textContent = numVisits;
-} else {
+if ((numVisits = 0)) {
   visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+} else {
+  visitsDisplay.textContent = numVisits;
 }
 
 numVisits++;
